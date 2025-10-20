24f95a678c8d0970b69c2c3fd93ea9f4830090ef
mhamad raad <hamaraad883@gmail.com>
2025-10-20 23:33:28 +0300

    update login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,35 +1,29 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent } from '@/components/ui/card';
 import { Field, FieldGroup, FieldLabel } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
-import { Mail, Lock, Eye, EyeOff } from 'lucide-react'; // lucid-react imports
-import Logo from '@/assets/Logo.jpg';
 
-const Login: React.FC = () => {
+import Logo from '@/assets/Logo.jpg';
+import { Eye, EyeOff, Mail, Lock } from 'lucide-react';
+
+const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className='w-full h-full flex items-center justify-center'>
-      <div className='w-[400px] flex flex-col gap-6 mt-40'>
-        <Card className='rounded-md'>
-          <CardHeader className='flex flex-col items-center'>
-            <img src={Logo} alt='Companies Logo' className='w-25 h-25' />
-            <CardTitle className='font-black'>Welcome Back !</CardTitle>
-            <CardDescription>
-              Please enter your credentials to login!
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <FieldGroup className='gap-5'>
-                <Field className='gap-1'>
+    <div className='w-full h-[100vh] flex items-center justify-center'>
+      <div className='w-[90%] max-w-[1000px] flex flex-col gap-6'>
+        <Card className='overflow-hidden p-0'>
+          <CardContent className='grid p-0 md:grid-cols-2'>
+            <form className='p-6 md:p-8'>
+              <FieldGroup>
+                <div className='flex flex-col items-center gap-2 text-center'>
+                  <h1 className='text-2xl font-bold'>Welcome back!</h1>
+                  <p className='text-muted-foreground text-balance'>
+                    Login to your account
+                  </p>
+                </div>
+                <Field className='gap-2'>
                   <FieldLabel htmlFor='email' className='text-muted-foreground'>
                     Email
                   </FieldLabel>
@@ -37,52 +31,63 @@ const Login: React.FC = () => {
                     <Input
                       id='email'
                       type='email'
-                      placeholder='E-mail'
+                      placeholder='m@example.com'
+                      className='h-11 pl-10' /* left padding for icon */
                       required
-                      className='pl-10 h-11'
                     />
-                    <span className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
-                      <Mail size={16} />
+                    <span className='absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground'>
+                      <Mail size={20} />
                     </span>
                   </div>
                 </Field>
-
-                <Field className='gap-1'>
-                  <FieldLabel
-                    htmlFor='password'
-                    className='text-muted-foreground'
-                  >
-                    Password
-                  </FieldLabel>
+                <Field className='gap-2'>
+                  <div className='flex items-center'>
+                    <FieldLabel
+                      htmlFor='password'
+                      className='text-muted-foreground'
+                    >
+                      Password
+                    </FieldLabel>
+                    <a
+                      href='#'
+                      className='ml-auto text-sm underline-offset-2 hover:underline'
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
                   <div className='relative'>
                     <Input
                       id='password'
                       type={showPassword ? 'text' : 'password'}
-                      placeholder='Password'
+                      className='h-11 pl-10 pr-10' /* left/right padding for icons */
+                      placeholder='******'
                       required
-                      className='pl-10 pr-10 h-11'
                     />
-                    <span className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
-                      <Lock size={16} />
+                    <span className='absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground'>
+                      <Lock size={20} />
                     </span>
                     <button
                       type='button'
-                      className='absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400'
-                      onClick={() => setShowPassword((prev) => !prev)}
                       tabIndex={-1}
+                      onClick={() => setShowPassword((v) => !v)}
+                      className='absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground'
                     >
-                      {showPassword ? <EyeOff size={22} /> : <Eye size={22} />}
+                      {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                     </button>
                   </div>
                 </Field>
-
-                <Field className='mt-5'>
-                  <Button type='submit' className='h-10 font-black text-[17px]'>
-                    Login
-                  </Button>
+                <Field>
+                  <Button type='submit'>Login</Button>
                 </Field>
               </FieldGroup>
             </form>
+            <div className='bg-muted relative hidden md:block'>
+              <img
+                src={Logo}
+                alt='Image'
+                className='absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale'
+              />
+            </div>
           </CardContent>
         </Card>
       </div>
