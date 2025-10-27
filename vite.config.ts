7526434a3ae1e484bb154589b35013e35a086a32
mhamad raad <hamaraad883@gmail.com>
2025-10-27 22:16:49 +0300

    delete unneeded packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,13 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import mkcert from 'vite-plugin-mkcert';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
-  server: {
-    https: true,
-    port: 5173,
-  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 });
-
