4dc14626641afa789a6fe6d52dd1fa4111e77098
mhamad raad <hamaraad883@gmail.com>
2025-10-29 19:41:21 +0300

    delete unneeded code

--- src/pages/users/UserDetail.tsx
+++ src/pages/users/UserDetail.tsx
@@ -1,8 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { ArrowLeft } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Card, CardDescription, CardTitle } from '@/components/ui/card';
 
 import UserDetailHeader from '@/components/Users/UserDetail/UserDetailHeader';
 import UserProfileCard from '@/components/Users/UserDetail/UserProfileCard';
