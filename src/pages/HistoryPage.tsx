5ab1c2aade963475a74260826cfcf4838e8a0240
Renua Hassan <RenuaCode123@hotmail.com>
2025-12-26 02:27:27 +0100

    refactor: simplify HistoryPage by removing unused selector and adjusting limit handling

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,18 +1,18 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import HistoryFilters from '@/components/History/HistoryFilters';
 import HistoryTable from '@/components/History/HistoryTable';
 import { fetchAuditLogs } from '@/store/slices/auditSlice';
-import type { AppDispatch, RootState } from '@/store/store';
+import type { AppDispatch } from '@/store/store';
 
 const HistoryPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const [searchParams] = useSearchParams();
-  const { limit } = useSelector((state: RootState) => state.audit);
 
   useEffect(() => {
     const page = Number(searchParams.get('page')) || 1;
+    const limit = Number(searchParams.get('limit')) || 40;
     const entityName = searchParams.get('entityName') || undefined;
     const fromDate = searchParams.get('fromDate') || undefined;
     const toDate = searchParams.get('toDate') || undefined;
@@ -26,7 +26,7 @@ const HistoryPage = () => {
         toDate,
       })
     );
-  }, [dispatch, searchParams, limit]);
+  }, [dispatch, searchParams]);
 
   return (
     <section className='w-full h-full flex flex-col gap-4 overflow-hidden'>
